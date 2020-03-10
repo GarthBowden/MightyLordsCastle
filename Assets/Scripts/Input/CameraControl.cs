b944e1b7a412e3711d3e274e3d89a882141e6f17
GarthBowden <garthbowden@gmail.com>
2020-03-10 12:11:41 -0700

    Add singleton script for camera control with rotate view method

---
+++ Assets/Scripts/Input/CameraControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    //Enforce itself as a singleton
+    public static CameraControl Instance { get; private set; }
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.Log("Duplicate " + this + " detected. Deleting myself!");
+            Destroy(this);
+        }
+    }
+
+    //Camera Rotation. Center Axis is the camera's focus point in the scene
+    public Vector3 CameraCenterAxis;
+    public void RotateViewCW()
+    {
+        gameObject.transform.RotateAround(CameraCenterAxis, Vector3.up, 90);
+    }
+
+}
